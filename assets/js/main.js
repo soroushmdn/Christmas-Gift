4fc37b7fcc999591caf54bc16a5eb2412a9ae2bd
Soroush Karimi <sorooshkarimi93@gmail.com>
2022-07-04 17:39:39 +0430

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,17 @@
-/*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+/*@@@@@@@@@@@@@@@@@ SHOW MENU @@@@@@@@@@@@@@@@@*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
 
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
+});
 
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+/*@@@@@@@@@ remove menu when click on nav links @@@@@@@@@@*/
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach((n) => {
+  n.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+});
